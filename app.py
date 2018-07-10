a54dfe3e8ada48b4f6412d93b029f132af5ebfd1
Logan Serio <Loganserio@gmail.com>
2018-07-09 19:37:26 -0500

    add form

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test')
-def test():
+@app.route('/twittersearch')
+def twittersearch():
     return """
-        <h1>The test worked</h1>
+        <form method="POST">
+            First name:<br>
+            <input type="text" name="firstname" value="test"><br>
+            Last name:<br>
+            <input type="text" name="lastname" value="test"><br><br>
+            <input type="submit" value="Submit">
+        </form>
     """
 
 @app.route('/')
@@ -13,7 +19,7 @@ def index():
     return """
     <h1>Welcome to the home of my micro apps!</h1>
     <p>On this webpage I will be creating and linking to small python applications that I've made.</p>
-    <a href="/test">Click here to go to test page</a>
+    <a href="/twittersearch">Click here to go to test page</a>
     """
 if __name__ == "__main__":
     app.run(debug=True,use_reloader=True)
