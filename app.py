56c6ecfec1b00214555bbe048aa87e08c7a4ce87
Logan Serio <Loganserio@gmail.com>
2018-07-09 19:07:24 -0500

    add test page

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/test')
+def test():
+    return """
+        <h1>The test worked</h1>
+    """
+
 @app.route('/')
 def index():
-    return 'Homepage'
+    return """
+    <h1>Welcome to the home of my micro apps!</h1>
+    <p>On this webpage I will be creating and linking to small python applications that I've made.</p>
+    <a href="heroku-micro-apps.herokuapp.com/test">Click here to go to test page</a>
+    """
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True,use_reloader=True)
