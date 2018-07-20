e0cf1caee7a098092bf64c168cde257241c5501d
Logan Serio <Loganserio@gmail.com>
2018-07-20 18:34:41 -0500

    mostly broken but needed to push

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template,request
+import twint
+import time
+import os
+import pandas as pd
+
+#postgresql-convex-23455
 
 app = Flask(__name__)
 
 @app.route('/twittersearch')
-def twittersearch():
-    return """
-        <form method="POST">
-            First name:<br>
-            <input type="text" name="firstname" value="test"><br>
-            Last name:<br>
-            <input type="text" name="lastname" value="test"><br><br>
-            <input type="submit" value="Submit">
-        </form>
-    """
+def twittersearchpage():
+    return render_template('twittersearch.html')
+
+@app.route('/searchresults',methods=['POST','GET'])
+def searchResults():
+    # if request.method == 'POST':
+    filename = "static/twitter.csv"
+    # # #opening the file with w+ mode truncates the file
+    f = open(filename, "w+")
+    f.truncate()
+    f.close()
+    c = twint.Config()
+    c.Username = request.form['username']
+    c.Store_csv = True
+    c.Custom = ["id", "user_id", "username", "tweet"] 
+    c.Limit = 20
+    c.Output = "static/twitter.csv"
+    twint.run.Search(c)
+    df = pd.read_csv('static/twitter.csv')
+    df.to_html("templates/blah.html")
+    return render_template('blah.html')
+
 
 @app.route('/')
 def index():
-    return """
-    <h1>Welcome to the home of my micro apps!</h1>
-    <p>On this webpage I will be creating and linking to small python applications that I've made.</p>
-    <a href="/twittersearch">Click here to go to test page</a>
-    """
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True,use_reloader=True)
